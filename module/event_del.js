5042baa8bc5b41188abfe45d1145c3de191879ed
Alone88 <anhaoyl@qq.com>
2022-03-26 18:26:22 +0800

    fix url error

--- module/event_del.js
+++ module/event_del.js
@@ -4,7 +4,7 @@ module.exports = (query, request) => {
   const data = {
     id: query.evId,
   }
-  return request('POST', `https://music.163.com/eapi/event/delete`, data, {
+  return request('POST', `https://music.163.com/weapi/event/delete`, data, {
     crypto: 'weapi',
     cookie: query.cookie,
     proxy: query.proxy,
